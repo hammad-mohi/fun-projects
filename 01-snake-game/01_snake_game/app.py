50094d6dafc95554940b1d501159b9e3f07bfeb3
Hammad Mohiuddin <hammad.mohiuddin@gmail.com>
2023-12-25 01:28:29 -0600

    Added Stats, Fixed Grid Movement

--- 01-snake-game/01_snake_game/app.py
+++ 01-snake-game/01_snake_game/app.py
@@ -12,6 +12,10 @@ DIR_DOWN = "DOWN"
 DIR_RIGHT = "RIGHT"
 DIR_LEFT = "LEFT"
 
+SPEED = 200
+
+GRID_SIZE = 20
+
 # Initial setup
 pygame.init()
 pygame.display.set_caption('Snake Potty Chutharr')
@@ -19,10 +23,12 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+font = pygame.font.Font('freesansbold.ttf', 18)
 
 player_dir = DIR_RIGHT
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-potty_pos = pygame.Vector2(random.random() * (screen.get_width() - BORDER_PADDING), random.random() * (screen.get_height() - BORDER_PADDING))
+
+player_pos = pygame.Vector2(screen.get_width() // 2, screen.get_height() // 2)
+potty_pos = pygame.Vector2(round(random.random() * (screen.get_width() - BORDER_PADDING)), round(random.random() * (screen.get_height() - BORDER_PADDING)))
 
 while running:
     # poll for events
@@ -33,10 +39,6 @@ while running:
 
     # Clear the screen
     screen.fill(BG_COLOR)
-
-    # Draw snake
-    pygame.draw.circle(screen, PLAYER_COLOR, player_pos, 20)
-    # pygame.draw.circle(screen, POTTY_COLOR, potty_pos, 20)
 
     keys = pygame.key.get_pressed()
 
@@ -50,14 +52,18 @@ while running:
     elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         player_dir = DIR_RIGHT
 
+    # Move the player
     if player_dir == DIR_UP and player_pos.y > BORDER_PADDING:
-        player_pos.y -= 300 * dt
-    if player_dir == DIR_DOWN and player_pos.y < screen.get_height() - BORDER_PADDING:
-        player_pos.y += 300 * dt
-    if player_dir == DIR_LEFT and  player_pos.x > BORDER_PADDING:
-        player_pos.x -= 300 * dt
-    if player_dir == DIR_RIGHT and player_pos.x < screen.get_width() - BORDER_PADDING:
-        player_pos.x += 300 * dt
+        player_pos.y -= 40
+    elif player_dir == DIR_DOWN and player_pos.y < screen.get_height() - BORDER_PADDING:
+        player_pos.y += 40
+    elif player_dir == DIR_LEFT and  player_pos.x > BORDER_PADDING:
+        player_pos.x -= 40
+    elif player_dir == DIR_RIGHT and player_pos.x < screen.get_width() - BORDER_PADDING:
+        player_pos.x += 40
+
+    # player_pos.x = GRID_SIZE * round(player_pos.x / GRID_SIZE)
+    # player_pos.y = GRID_SIZE * round(player_pos.y / GRID_SIZE)
 
     # TODOs:
     # // 0. Move in direction of last key
@@ -78,13 +84,20 @@ while running:
     # 3. Game Play - Play Game
     # 4. End Game - Die, Restart, Home
 
+    # Draw HUD
+    player_text = font.render(f"Player: ({player_pos.x}, {player_pos.y})", True, PLAYER_COLOR, BG_COLOR)
+    potty_text = font.render(f"Potty: ({potty_pos.x}, {potty_pos.y})", True, POTTY_COLOR, BG_COLOR)
+    screen.blit(player_text, (0,0))
+    screen.blit(potty_text, (0,20))
+
+    if potty_pos.x == player_pos.x and potty_pos.y == player_pos.y:
+        potty_pos = pygame.Vector2(round(random.random() * (screen.get_width() - BORDER_PADDING)), round(random.random() * (screen.get_height() - BORDER_PADDING)))
+
+    # Draw snake
+    pygame.draw.circle(screen, PLAYER_COLOR, player_pos, GRID_SIZE)
+    pygame.draw.circle(screen, POTTY_COLOR, potty_pos, GRID_SIZE)
 
     # 1. Potty Generation
-
-    pygame.draw.circle(screen, PLAYER_COLOR, player_pos, 20)
-
-
-
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -92,6 +105,7 @@ while running:
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
-    dt = clock.tick(120) / 1000
+    dt = clock.tick(60) / 1000
+
 
 pygame.quit()
