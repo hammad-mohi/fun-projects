728e3ac4ededa19a0d3c11d742a8365ca7298824
Hammad Mohiuddin <hammad.mohiuddin@gmail.com>
2024-01-01 15:46:52 -0800

    Adding body parts to snake on eating apples

--- 01-snake-game/01_snake_game/app.py
+++ 01-snake-game/01_snake_game/app.py
@@ -26,12 +26,20 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
-font = pygame.font.Font('freesansbold.ttf', 18)
+font = pygame.font.Font('freesansbold.ttf', 15)
 
 player_dir = DIR_RIGHT
-player_pos = pygame.Vector2(screen.get_width() // 2, screen.get_height() // 2)
-potty_pos = pygame.Vector2(screen.get_width() // 2, screen.get_height() // 2)
-score = -1
+player_pos = [pygame.Vector2(screen.get_width() // 2, screen.get_height() // 2)]
+potty_pos = pygame.Vector2(
+    BORDER_PADDING + random.random() * (screen.get_width() - 2 * BORDER_PADDING),
+    BORDER_PADDING + random.random() * (screen.get_height() - 2 * BORDER_PADDING)
+)
+# Snap potty's x-coordinate to the nearest multiple of GRID_SIZE
+potty_pos.x = math.floor(potty_pos.x / GRID_SIZE) * GRID_SIZE
+# Snap potty's y-coordinate to the nearest multiple of GRID_SIZE
+potty_pos.y = math.floor(potty_pos.y / GRID_SIZE) * GRID_SIZE
+
+score = 0
 speed = 1
 
 while running:
@@ -41,13 +49,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    if player_pos.x >= screen.get_width() - BORDER_PADDING:
+    # Checks if snake goes out of bounds
+    if player_pos[0].x >= screen.get_width() - BORDER_PADDING:
         break
-    elif player_pos.y >= screen.get_height() - BORDER_PADDING:
+    elif player_pos[0].y >= screen.get_height() - BORDER_PADDING:
         break
-    elif player_pos.y <= BORDER_PADDING:
+    elif player_pos[0].y <= BORDER_PADDING:
         break
-    elif player_pos.x <= BORDER_PADDING:
+    elif player_pos[0].x <= BORDER_PADDING:
         break
 
     # Clear the screen
@@ -65,15 +74,22 @@ while running:
     elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         player_dir = DIR_RIGHT
 
+    last_player_pos = pygame.Vector2(player_pos[0].x, player_pos[0].y)
+
     # Move the player
-    if player_dir == DIR_UP and player_pos.y > BORDER_PADDING:
-        player_pos.y -= GRID_SIZE
-    elif player_dir == DIR_DOWN and player_pos.y < screen.get_height() - BORDER_PADDING:
-        player_pos.y += GRID_SIZE
-    elif player_dir == DIR_LEFT and  player_pos.x > BORDER_PADDING:
-        player_pos.x -= GRID_SIZE
-    elif player_dir == DIR_RIGHT and player_pos.x < screen.get_width() - BORDER_PADDING:
-        player_pos.x += GRID_SIZE
+    if player_dir == DIR_UP and player_pos[0].y > BORDER_PADDING:
+        for body in player_pos:
+            body.y -= GRID_SIZE
+    elif player_dir == DIR_DOWN and player_pos[0].y < screen.get_height() - BORDER_PADDING:
+        for body in player_pos:
+            body.y += GRID_SIZE
+    elif player_dir == DIR_LEFT and  player_pos[0].x > BORDER_PADDING:
+        for body in player_pos:
+            body.x -= GRID_SIZE
+    elif player_dir == DIR_RIGHT and player_pos[0].x < screen.get_width() - BORDER_PADDING:
+        for body in player_pos:
+            body.x += GRID_SIZE
+
 
     # TODOs:
     # // 0. Move in direction of last key
@@ -95,28 +111,46 @@ while running:
     # 4. End Game - Die, Restart, Home
 
     # Draw HUD
-    player_text = font.render(f"Player: ({player_pos.x}, {player_pos.y})", True, PLAYER_COLOR, BG_COLOR)
+    player_text = font.render(f"Player: ({player_pos[0].x}, {player_pos[0].y})", True, PLAYER_COLOR, BG_COLOR)
     potty_text = font.render(f"Potty: ({potty_pos.x}, {potty_pos.y})", True, POTTY_COLOR, BG_COLOR)
     score_text = font.render(f"Score: {score}", True, POTTY_COLOR, BG_COLOR)
+    length_text = font.render(f"Length: {len(player_pos)}", True, PLAYER_COLOR, BG_COLOR)
     screen.blit(player_text, (0,0))
     screen.blit(potty_text, (0,20))
     screen.blit(score_text, (0,40))
-    if abs(potty_pos.x - player_pos.x) <= GRID_SIZE and  abs(potty_pos.y - player_pos.y) <= GRID_SIZE:
+    screen.blit(length_text, (0,60))
+
+    # Check if the potty is in the grid size range of the snake
+    if abs(potty_pos.x - player_pos[0].x) <= GRID_SIZE and  abs(potty_pos.y - player_pos[0].y) <= GRID_SIZE:
+        player_pos.append(last_player_pos)
+
         # Randomly reposition potty within the screen bounds, avoiding the border
         potty_pos = pygame.Vector2(
-            random.random() * (screen.get_width() - BORDER_PADDING),
-            random.random() * (screen.get_height() - BORDER_PADDING)
+            BORDER_PADDING + random.random() * (screen.get_width() - 2 * BORDER_PADDING),
+            BORDER_PADDING + random.random() * (screen.get_height() - 2 * BORDER_PADDING)
         )
-
         # Snap potty's x-coordinate to the nearest multiple of GRID_SIZE
         potty_pos.x = math.floor(potty_pos.x / GRID_SIZE) * GRID_SIZE
         # Snap potty's y-coordinate to the nearest multiple of GRID_SIZE
         potty_pos.y = math.floor(potty_pos.y / GRID_SIZE) * GRID_SIZE
+
+        # # Gets the location of where the next snake body will be and appends it
+        # if player_dir == DIR_RIGHT:
+        #     player_pos.append(pygame.Vector2(player_pos[0].x - GRID_SIZE, player_pos[0].y))
+        # elif player_dir == DIR_LEFT:
+        #     player_pos.append(pygame.Vector2(player_pos[0].x + GRID_SIZE, player_pos[0].y))
+        # elif player_dir == DIR_DOWN:
+        #     player_pos.append(pygame.Vector2(player_pos[0].x , player_pos[0].y - GRID_SIZE))
+        # elif player_dir == DIR_UP:
+        #     player_pos.append(pygame.Vector2(player_pos[0].x , player_pos[0].y + GRID_SIZE))
+
         score += 1
         speed += 0.01
 
     # Draw snake
-    pygame.draw.circle(screen, PLAYER_COLOR, player_pos, PLAYER_SIZE)
+    for i, body in enumerate(player_pos):
+        # print(i, body)
+        pygame.draw.circle(screen, PLAYER_COLOR, body, PLAYER_SIZE)
     pygame.draw.circle(screen, POTTY_COLOR, potty_pos, POTTY_SIZE)
 
     # 1. Potty Generation
